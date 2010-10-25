8092d0e81ad71c011b6852a8234c4f50bc51ef0e
Tamás Benke <tombenke@gmail.com>
2010-10-25 22:03:58 +0200

    rearranging the templating for listgen

--- _design/cogito/lists/contacts.js
+++ _design/cogito/lists/contacts.js
@@ -1,31 +1,7 @@
 function( head, req )
-{  
-    var ddoc = this;
-    var Mustache = require( "vendor/couchapp/lib/mustache" );
-    var path = require( "vendor/couchapp/lib/path" ).init( req );
-    var List = require("vendor/droids/lib/listgen");
+{
+    var listgen = require( "vendor/droids/lib/listgen" );
 
-    provides( 'html', function()
-    {
-        send( Mustache.to_html(
-            ddoc.templates.contacts,
-            {
-                header : {assets : path.asset()},
-                top_menu : {},
-                left_sidebar : {},
-                main : {},
-                right_sidebar : {},
-                scripts : {},
-                footer : {},
-                head : head,
-                req : req,
-                rows : head.rows,
-                userCtx : head.userCtx,
-                assets : path.asset(),
-                contact_list : List.renderItem()
-            },
-            ddoc.templates.partials
-        ));
-    });
+    listgen.to_template( head, req, this, "contacts" );
 }
 
--- _design/cogito/lists/projects.js
+++ _design/cogito/lists/projects.js
@@ -1,30 +1,6 @@
 function( head, req )
-{  
-    var ddoc = this;
-    var Mustache = require( "vendor/couchapp/lib/mustache" );
-    var path = require( "vendor/couchapp/lib/path" ).init( req );
-    var List = require("vendor/droids/lib/listgen");
+{
+    var listgen = require( "vendor/droids/lib/listgen" );
 
-    provides( 'html', function()
-    {
-        send( Mustache.to_html(
-            ddoc.templates.projects,
-            {
-                header : {assets : path.asset()},
-                top_menu : {},
-                left_sidebar : {},
-                main : {},
-                right_sidebar : {},
-                scripts : {},
-                footer : {},
-                head : head,
-                req : req,
-                rows : head.rows,
-                userCtx : head.userCtx,
-                assets : path.asset(),
-                project_list : List.renderItem()
-            },
-            ddoc.templates.partials
-        ));
-    });
+    listgen.to_template( head, req, this, "projects" );
 }
